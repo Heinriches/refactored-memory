0eb24b19dddf22c3a2eb2bad172ba5493c91c504
Heinriches <heydrich.institut@gmail.com>
2026-05-12 12:28:32 -0300

    fix: Exercicio_008.java
    fix missing reference to 'dist' variable in line 12

--- FH/Java/Exercicio_008/Exercicio_008.java
+++ FH/Java/Exercicio_008/Exercicio_008.java
@@ -9,7 +9,7 @@ class Exercicio_008
 		System.out.print("Digite uma distância, em metros: ");
 		double dist = sc.nextDouble();
 		
-		System.out.printf("A distância de %f m corresponde a: \n");
+		System.out.printf("A distância de %f m corresponde a: \n" ,dist);
 		System.out.printf("%,.2f Km. \n" ,(dist / 1000));
 		System.out.printf("%,.2f cm. \n" ,(dist * 100));
 		System.out.printf("%,.2f mm. \n" ,(dist * 1000));
