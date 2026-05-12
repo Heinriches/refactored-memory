499370d7f740b1a45746fca89eb2f20c9daa9b41
Heinriches <heydrich.institut@gmail.com>
2026-05-12 12:09:42 -0300

    feat: create Exercicio_008.java

---
+++ FH/Java/Exercicio_008/Exercicio_008.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Exercicio_008
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite uma distância, em metros: ");
+		double dist = sc.nextDouble();
+		
+		System.out.printlf("A distância de %d m corresponde a: \n");
+		System.out.println((dist / 1000) +"Km.");
+		System.out.println((dist * 100) +"cm.");
+		System.out.println((dist * 1000) +"mm.");
+		
+		
+		sc.close();
+	}
+}
