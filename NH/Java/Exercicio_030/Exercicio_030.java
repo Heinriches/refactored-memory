cb1c4a32a1698f310166bc9a228de742fbb5b356
Nikolas <131282424+N1k0145@users.noreply.github.com>
2026-05-21 18:24:41 -0300

    Add files via upload

---
+++ NH/Java/Exercicio_030/Exercicio_030.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+class Exercicio_030{
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite o comprimento do segmento a: ");
+		int a = sc.nextInt();
+		
+		System.out.print("Digite o comprimento do segmento b: ");
+		int b = sc.nextInt();
+		
+		System.out.print("Digite o comprimento do segmento c: ");
+		int c = sc.nextInt();
+		
+		if(a < (b + c)){
+			if(b < (a + c)){
+				if(c < (a + b)){
+					System.out.println("É possivel formar um triângulo com essas retas");
+		
+					if(a == b && b == c){
+						System.out.println("TRIANGULO EQUILATERO");
+					}else if(a != b && b == c){
+						System.out.println("TRIANGULO ISOSCELES");
+						}else{
+							System.out.println("TRIANGULO ESCALENO");
+						}
+						
+				}else{
+					System.out.println("Não é possivel formar um triângulo com essas retas");
+				}
+			}else{
+				System.out.println("Não é possivel formar um triângulo com essas retas");
+			}
+			
+		}else{
+			System.out.println("Não é possivel formar um triângulo com essas retas");
+		}
+		
+		
+		
+		sc.close();
+	}
+	
+	
+}
