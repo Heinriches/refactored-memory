95afc52e7921bb58512a46df2249b99ada4f1561
Nikolas <131282424+N1k0145@users.noreply.github.com>
2026-06-10 20:26:34 -0300

    Add files via upload

---
+++ NH/Java/Exercicio_039/Exercicio_039.java
@@ -0,0 +1,12 @@
+class Exercicio_039{
+	
+	public static void main(String[] args){
+		
+		for(int i = 10; i >= 3; i--){
+			System.out.print(i + " ");
+		}
+			System.out.print("Acabou!");
+		
+	}
+	
+}
