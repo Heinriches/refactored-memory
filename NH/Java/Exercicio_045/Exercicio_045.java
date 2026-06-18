11b7ffac695326d16cbe2e7ef93475a61550b92f
Nikolas <131282424+N1k0145@users.noreply.github.com>
2026-06-18 20:18:44 -0300

    Add files via upload

---
+++ NH/Java/Exercicio_045/Exercicio_045.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class Exercicio_045{
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite o primeiro valor: ");
+		int ini = sc.nextInt();
+		
+		System.out.print("Digite o ultimo valor: ");
+		int ulti = sc.nextInt();
+		
+		System.out.print("Digite o incremento valor: ");
+		int incre = sc.nextInt();
+		
+		
+		if(ini >= ulti){
+			for(int i = ini; i >= ulti; i -= incre){
+				System.out.print(i + " ");
+			}
+		}else if(ini <= ulti){
+			for(int i = ini; i <= ulti; i += incre){
+				System.out.print(i + " ");
+			}
+		}else{
+			System.out.print(ulti);
+		}
+			
+		sc.close();
+	}
+	
+}
